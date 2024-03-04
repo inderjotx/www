7a8f9b6c0bd1ce23b6875ab251180610acae3f48
Inderjot Singh <inderjotsingh141@gmail.com>
2024-03-04 20:43:36 +0530

    working on carousel

--- src/app/_components/Dashboard/tools-carousel/index.tsx
+++ src/app/_components/Dashboard/tools-carousel/index.tsx
@@ -3,38 +3,51 @@ import React from 'react'
 
 import {
   IconJava, IconJenkins, IconNextJS, IconKubernetes,
-  IconLambda, IconPrisma, IconPostgres, IconPython, IconReactJS, IconRust,
+  IconLambda, IconPrisma, IconPostgres, IconPython, IconReactJS,
   IconTailwindcss, IconTerraform, IconTypescript, IconVite,
   IconAWS, IconDocker, IconGit, IconMySQL
 } from '@/components/Icons'
+import { cn } from '@/lib/utils';
 
 
 const icons = [
   IconJava, IconJenkins, IconNextJS, IconKubernetes,
-  IconLambda, IconPrisma, IconPostgres, IconPython, IconReactJS, IconRust,
+  IconLambda, IconPrisma, IconPostgres, IconPython, IconReactJS,
   IconTailwindcss, IconTerraform, IconTypescript, IconVite,
-  IconAWS, IconDocker, IconGit, IconMySQL
+  IconAWS, IconGit, IconMySQL
 ];
+
+
 
 
 
 export function Tools() {
 
+  const reversedIcons = [...icons]
+  reversedIcons.reverse()
+
+
   return (
-    <div className='w-full h-full  ' >
-      <Carousel icons={icons} />
+    <div className='w-full h-full flex flex-col gap-3 justify-center relative ' >
+      <Carousel icons={icons} direction='left' />
+      <Carousel icons={reversedIcons} direction='right' />
+
     </div>
   )
 }
 
 
 
-export function Carousel({ icons }: { icons: any }) {
+export function Carousel({ icons, direction }: { icons: any, direction: "left" | "right" }) {
+
+
+  const effect = direction == "left" ? "animate-slide_left" : "animate-slide_right"
+  const styleDir = direction == "right" ? { "direction": "rtl" } : { "direction": "left" }
 
 
   return (
-    <div className='overflow-hidden whitespace-nowrap  relative' >
-      <div className='flex gap-4 animate-slide ' >
+    <div className={cn('overflow-hidden whitespace-nowrap relative ')} style={styleDir}  >
+      <div className={cn('flex gap-6 ', effect)} >
         {
           icons.map((Icon: any, index: number) => (
             <IconWrapper key={index} >
@@ -42,8 +55,8 @@ export function Carousel({ icons }: { icons: any }) {
             </IconWrapper>
           ))
         }
-      </div>
 
+      </div>
     </div>
   )
 }
@@ -53,6 +66,15 @@ function IconWrapper({ children }: { children: React.ReactNode }) {
   return (
     <div className='size-10  bg-white p-1 flex items-center justify-center rounded-sm '>
       {children}
+    </div>
+  )
+}
+
+
+function Fade() {
+  return (
+    <div className='absolute h-full top-0 w-1/5'>
+
     </div>
   )
 }
