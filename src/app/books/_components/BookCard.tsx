32060227d88be572f3ec90c311015a273f485085
Inderjot Singh <inderjotsingh141@gmail.com>
2024-03-09 13:50:08 +0530

    imporves in ui

--- src/app/books/_components/BookCard.tsx
+++ src/app/books/_components/BookCard.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import { Book } from '@/interfaces/music/books'
-import { BookAudio } from 'lucide-react'
-import { parseDate } from '@/lib/utils'
+import { BookAudio, BookOpenText, BookText } from 'lucide-react'
+import { cn, parseDate } from '@/lib/utils'
 
 export function BookCard({
     id,
@@ -17,7 +17,7 @@ export function BookCard({
 ) {
     return (
         <a target='_blank' href={`https://literal.club/book/${slug}`} >
-            <div className='w-full h-20 active:ring active:ring-purple-400 bg-muted-foreground/10  rounded-md flex'>
+            <div className={cn('w-full  active:ring active:ring-purple-400 bg-muted-foreground/10  rounded-sm flex', startedReading ? "h-20" : "h-16")}>
                 <div className='w-1/6 h-full rounded-l-sm overflow-hidden relative' >
                     <Image className='object-cover' alt='image' quality={100} src={cover} fill sizes='100' ></Image>
                 </div>
@@ -32,15 +32,16 @@ export function BookCard({
                             </>
                         }
                     </div>
-                    <div className='font-semibold flex gap-1' >
-                        <span>
+                    <div className='font-semibold text-sm flex gap-1' >
+
+                        <span className='truncate'>
                             {title}
                         </span>
-                        <div className='text-[10px] rounded-[4px] self-end p-[3px] bg-muted-foreground/20' >
+                        <div className='text-[10px] rounded-[4px] self-end p-[2px] bg-muted-foreground/20' >
                             {new Date(publishedDate).getFullYear()}
                         </div>
                     </div>
-                    <div className='text-sm text-muted-foreground'>
+                    <div className='text-[12px] text-muted-foreground'>
                         {authors[0].name}
                     </div>
                     <div>
@@ -51,3 +52,36 @@ export function BookCard({
     )
 }
 
+
+export function LoadingBookCard(
+) {
+    return (
+        <a target='_blank' href={`/books`} >
+            <div className={cn('w-full  active:ring active:ring-purple-400 bg-muted-foreground/10  rounded-sm flex h-20')}>
+                <div className='w-1/6 h-full bg-muted-foreground/10 flex items-center justify-center rounded-l-sm overflow-hidden relative' >
+                    <BookOpenText className='size-8' />
+                </div>
+                <div className='w-5/6 p-2 justify-center pl-5 h-full flex flex-col gap-1' >
+                    <div className='text-[12px]  font-sans  flex items-center  gap-2 text-purple-400 '>
+                        <>
+                            <BookAudio className='size-4' /> <span >
+                                {'Fetching...'}
+                            </span>
+                        </>
+                    </div>
+                    <div className='font-semibold text-sm flex gap-1' >
+
+                        <span className='truncate'>
+                            {'Title...'}
+                        </span>
+                    </div>
+                    <div className='text-[12px] text-muted-foreground'>
+                        {'Writer'}
+                    </div>
+                    <div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,8 +1,8 @@
-import { getTopTracks, getCurrentTrack, getRecentTrack } from '@/lib/favsongs'
+import { getTopTracks } from '@/lib/favsongs'
 import { poppins } from '@/lib/fonts/poppins'
 import { cn } from '@/lib/utils'
 import React, { Suspense } from 'react'
-import { LoadingRecent, RecentPlay, RecentPlayProps } from './_components/RecentFramer'
+import { RecentPlay } from './_components/MusicCard'
 import { SingleTrack } from './_components/SingleTracks'
 
 export default async function Page() {
@@ -17,7 +17,6 @@ export default async function Page() {
                 Music has always been something near to my heart. Whether it&apos;s a happy day or a sad one, there is a memory linked with it, and a song that accompanies the moment.
             </div>
 
-            {/* now playing or recent */}
             <div>
                 <RecentPlay />
             </div>
@@ -28,7 +27,6 @@ export default async function Page() {
                 I listen to a lot of Spotify, Over the last 12 months, Below you can find an up-to-date collection of my favourite songs from the past ~4 weeks.
             </div>
 
-            {/* { fav songs } */}
             <div className='h-40 gap-2 w-full whitespace-nowrap scrollbar-hide  flex overflow-x-auto  '>
 
 
