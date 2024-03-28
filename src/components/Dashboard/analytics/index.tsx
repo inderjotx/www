f71b06cf67b337762793fbe6fda01f9d2c6bd1ad
Inderjot Singh <inderjotsingh141@gmail.com>
2024-03-28 18:25:21 +0530

    signifact ui changes

--- src/components/Dashboard/analytics/index.tsx
+++ src/components/Dashboard/analytics/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { fetcher } from '@/lib/utils'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import useSWR from 'swr'
 
@@ -32,16 +33,18 @@ export function Analytics() {
         })
 
         return (
-            <div className='relative w-full  h-full flex '>
-                <div className='absolute font-mono  right-3 h-full text-sm text-center bg-black/60  font-medium  backdrop-blur-sm ' style={{ 'textOrientation': 'mixed', 'writingMode': 'vertical-rl' }}  >Since Last Hour </div>
-                <div className='flex pl-3  md:text-[15px] items-start  justify-center flex-col'>
-                    <div>  Clicks : {data.totalClicks} </div>
-                    <div> City  : {city} </div>
-                    <div> Country  : {country} </div>
+            <Link href={'/analytics'} >
+                <div className='relative w-full  h-full flex '>
+                    <div className='absolute font-mono  right-3 h-full text-sm text-center bg-black/60  font-medium  backdrop-blur-sm ' style={{ 'textOrientation': 'mixed', 'writingMode': 'vertical-rl' }}  >Since Last Hour </div>
+                    <div className='flex pl-3  md:text-[15px] items-start  justify-center flex-col'>
+                        <div>  Clicks : {data.totalClicks} </div>
+                        <div> City  : {city} </div>
+                        <div> Country  : {country} </div>
+                    </div>
+                    <Image quality={100} src={'/dashboard/analytics.png'} alt='cat girl showing analytics ' fill className='object-cover absolute inset-0 -z-20 ' sizes='100' />
+                    <div className='absolute h-full -z-10 w-full bg-black/70' />
                 </div>
-                <Image quality={100} src={'/dashboard/analytics.png'} alt='cat girl showing analytics ' fill className='object-cover absolute inset-0 -z-20 ' sizes='100' />
-                <div className='absolute h-full -z-10 w-full bg-black/70' />
-            </div>
+            </Link>
         )
     }
 }
