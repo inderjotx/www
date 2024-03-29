54d55b217bdc179c5b48f3ac4f204919b9d4671f
Inderjot Singh <inderjotsingh141@gmail.com>
2024-03-29 11:15:59 +0530

    update prisma client to use edge

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,44 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export const prismaClient = new PrismaClient().$extends(withAccelerate())
 
 
 
-// add client to the global instance
-declare global {
-    var prisma: (null | PrismaClient)
-}
 
 
-// create client if  
 
-if (process.env.NODE_ENV === 'production') {
-    globalThis.prisma = new PrismaClient()
-}
-else {
-    if (!globalThis.prisma) globalThis.prisma = new PrismaClient()
-}
 
-export const prismaClient = globalThis.prisma
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { PrismaClient } from '@prisma/client'
+
+
+
+// // add client to the global instance
+// declare global {
+//     var prisma: (null | PrismaClient)
+// }
+
+
+// // create client if  
+
+// if (process.env.NODE_ENV === 'production') {
+//     globalThis.prisma = new PrismaClient()
+// }
+// else {
+//     if (!globalThis.prisma) globalThis.prisma = new PrismaClient()
+// }
+
+// export const prismaClient = globalThis.prisma
